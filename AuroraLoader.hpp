c14d416a930c95c86f0d3f7d10649e2eaf66279c
JTuthill01 <45497162+JTuthill01@users.noreply.github.com>
2019-03-26 16:29:54 -0500

    Add files via upload

---
+++ AuroraLoader.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <Entity/Player/Player.hpp>
+#include <Entity/Enemies/EnemyTank.hpp>
+#include <Collision/Collision.hpp>
+
+namespace AuroraLoader
+{
+	class AuroraLoader
+	{
+	public:
+		AuroraLoader();
+		~AuroraLoader();
+
+		void loadAurora();
+		void setAurora(const float& deltaTime);
+		void render(sf::RenderTarget& target);
+		void update(const float& deltaTime);
+
+	private:
+		Player mPlayer;
+		EnemyTank mEnemy;
+		Collision mCollision;
+
+		sf::Texture mAuroraFireTexture;
+		sf::Texture mAuroraIceTexture;
+		sf::Texture mAuroraCorrosionTexture;
+		sf::Sprite mAuroraSprite;
+	};
+
+}
