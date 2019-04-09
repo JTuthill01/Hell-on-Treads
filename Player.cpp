1d65726cd9aea9df004501927b00228e146d13a4
Shadow <45497162+JTuthill01@users.noreply.github.com>
2019-04-09 10:10:08 -0500

    Add files via upload

--- Player.cpp
+++ Player.cpp
@@ -2,6 +2,7 @@
 #include "Player.hpp"
 
 std::vector<sf::Texture>Player::mPlayerProjectileTextures;
+std::vector<sf::Texture>Player::mPlayerTurretProjectileTextures;
 
 Player::Player()
 {
@@ -11,40 +12,134 @@ Player::Player()
 		std::cerr << "Failed to load texture" << "\n";
 
 	this->mPlayerSprite.setTexture(this->mPlayerTexture);
-	this->mPlayerSprite.setScale(1.7F, 1.7F);
-
-	this->mPlayerSprite.setOrigin(this->mPlayerSprite.getGlobalBounds().left + this->mPlayerSprite.getGlobalBounds().width / 2,
-		this->mPlayerSprite.getGlobalBounds().top + this->mPlayerSprite.getGlobalBounds().height / 2);
 
 	this->mPlayerSprite.setPosition(300.F, 710.F);
 
 	this->createMovementComponent(350.f, 16.f, 6.f);
 	this->createAnimationComponent(this->mPlayerTexture);
 
-	this->pAnimationComponent->addAnimation("MOVE", 5.f, 0, 0, 7, 0, 256, 256);
-	this->pAnimationComponent->addAnimation("ATTACK", 5.f, 0, 2, 7, 2, 256, 256);
+	this->pAnimationComponent->addAnimation("MOVE", 5.f, 0, 0, 7, 0, 435, 435);
+	this->pAnimationComponent->addAnimation("ATTACK", 5.f, 0, 1, 7, 1, 435, 435);
 
 	this->pAudio.loadSound("Resources/Sounds/Explosions/DeathFlash.flac");
 	this->pAudio.setVolume(20.F);
 
-	this->mCurrentWeapon = REGULAR;
+	this->pCurrentWeapon = REGULAR;
 
 	this->loadProjectile();
+
+	this->mPlayerCenter.x = this->mPlayerSprite.getPosition().x + this->mPlayerSprite.getGlobalBounds().width / 2;
+	this->mPlayerCenter.y = this->mPlayerSprite.getPosition().y + this->mPlayerSprite.getGlobalBounds().height / 2;
+
+	this->hpBar();
 }
 
 Player::~Player() = default;
 
-Projectile& Player::getProjectile(unsigned index) { return this->mProjectile[index]; }
-
-void Player::render(sf::RenderTarget & target) 
+const int Player::playerDealDamage() const
 {
-	target.draw(this->mPlayerSprite); 
+	int damage = 0;
 
-	for (size_t i = 0; i < this->mProjectile.size(); i++)
-		this->mProjectile[i].render(target);
+	switch (this->pCurrentWeapon)
+	{
+	case REGULAR:
+		damage = rand() % this->mDamage + this->mDamageMax;
+		break;
 
-	if(this->mIsMuzzleOn)
-		this->mProjectiles.render(target);
+	case FIRE:
+		damage = rand() % this->mDamage + this->mDamageMax + rand();
+		break;
+
+	case ICE:
+		damage = rand() % this->mDamage + this->mDamageMax;
+		damage *= rand() % 2;
+
+		break;
+
+	case CORROSIVE:
+		damage = rand() % this->mDamage + this->mDamageMax;
+		damage *= rand() % 3;
+
+	default:
+		break;
+	}
+
+	return damage;
+}
+
+void Player::createTurret()
+{
+	if (!this->mTurretTexture_0.loadFromFile("Resources/Textures/Player/Turret.png"))
+		std::cerr << "Fucking turret failed to load" << "\n";
+
+	this->mTurretSprite_0.setTexture(this->mTurretTexture_0);
+	this->mTurretSprite_0.setScale(1.F, 1.F);
+	this->mTurretSprite_0.setPosition(50.F, 710.F);
+}
+
+void Player::updateTurret(const float& deltaTime)
+{
+}
+
+void Player::renderTurret(sf::RenderTarget& target)
+{
+	target.draw(this->mTurretSprite_0);
+}
+
+void Player::loadTurretProjectiles()
+{
+}
+
+Projectile& Player::getPlayerProjectile(unsigned index) { return this->mPlayerProjectile[index]; }
+
+void Player::createSoldier()
+{
+	this->createMovementComponent(350.f, 16.f, 6.f);
+	this->createAnimationComponent(this->mSoldierTexture);
+
+	this->pAnimationComponent->addAnimation("MOVE", 5.f, 0, 0, 7, 0, 140, 140);
+
+	if (!this->mSoldierTexture.loadFromFile("Resources/Textures/Player/Soldier/Player_Soldier.png"))
+		std::cerr << "You suh can fuck right, as the player texture failed to load" << "\n";
+
+	this->mSoldierSprite.setPosition(0.F, 710.F);
+	this->mSoldierSprite.setScale(1.F, 1.F);
+}
+
+void Player::takeDamage(int damage)
+{
+	this->mHp -= damage;
+
+	if (this->mHp <= 0)
+	{
+		this->mHp = 0;
+
+		this->mIsAlive = false;
+	}
+}
+
+void Player::removePlayerProjectile(unsigned index) { this->mPlayerProjectile.erase(this->mPlayerProjectile.begin() + index); }
+
+void Player::render(sf::RenderTarget & target)
+{
+	target.draw(this->mPlayerSprite);
+
+	target.draw(this->mAuroaDyingSprite);
+
+	target.draw(this->mHpBar1);
+
+	for (size_t i = 0; i < this->mPlayerProjectile.size(); i++)
+		this->mPlayerProjectile[i].render(target);
+
+	if (this->mIsMuzzleOn)
+		this->mMuzzle.render(target);
+}
+
+void Player::hpBar()
+{
+	//Bar one
+	this->pHpBar1.setSize(sf::Vector2f(150.F, 22.F));
+	this->pHpBar1.setFillColor(sf::Color::Blue);
 }
 
 void Player::initVariables()
@@ -55,13 +150,11 @@ void Player::initVariables()
 
 	this->mIsAttacking = false;
 
-	this->mKeyTimeMax = 35.5F;
+	this->mKeyTimeMax = 55.5F;
 
 	this->mKeyTime = mKeyTimeMax;
 
-	this->mIsContact = false;
-
-	this->mShootTimerMax = 35.F;
+	this->mShootTimerMax = 55.F;
 
 	this->mShootTimer = this->mShootTimerMax;
 
@@ -70,25 +163,50 @@ void Player::initVariables()
 	this->mMuzzleTimerMax = 1.5F;
 
 	this->mMuzzleTimer = this->mMuzzleTimerMax;
+
+	this->mHp = 20;
+
+	this->mHpMax = 30;
+
+	this->mDamage = 2;
+
+	this->mDamageMax = 4;
+
+	this->mIsAlive = true;
 }
 
 void Player::currentWeapon(const float& deltaTime)
 {
-	if (this->mCurrentWeapon == REGULAR)
-		this->mProjectile.push_back(Projectile(&Player::mPlayerProjectileTextures[REGULAR], sf::Vector2f(this->mPlayerSprite.getPosition().x + 300, this->mPlayerSprite.getPosition().y + 195),
+	if (this->pCurrentWeapon == REGULAR)
+		this->mPlayerProjectile.push_back(Projectile(&Player::mPlayerProjectileTextures[REGULAR], sf::Vector2f(this->mPlayerSprite.getPosition().x + 300, this->mPlayerSprite.getPosition().y + 195),
 			sf::Vector2f(-0.4F, 0.5F), sf::Vector2f(1.F, 0.F)));
 
-	else if (this->mCurrentWeapon == FIRE)
-		this->mProjectile.push_back(Projectile(&Player::mPlayerProjectileTextures[FIRE], sf::Vector2f(this->mPlayerSprite.getPosition().x + 300, this->mPlayerSprite.getPosition().y + 195),
+	else if (this->pCurrentWeapon == FIRE)
+	{
+		this->mPlayerProjectile.push_back(Projectile(&Player::mPlayerProjectileTextures[FIRE], sf::Vector2f(this->mPlayerSprite.getPosition().x + 300, this->mPlayerSprite.getPosition().y + 195),
 			sf::Vector2f(-0.4F, 0.5F), sf::Vector2f(1.F, 0.F)));
 
-	else if (this->mCurrentWeapon == ICE)
-		this->mProjectile.push_back(Projectile(&Player::mPlayerProjectileTextures[ICE], sf::Vector2f(this->mPlayerSprite.getPosition().x + 300, this->mPlayerSprite.getPosition().y + 195),
+		for (size_t i = 0; i < this->mPlayerProjectile.size(); i++)
+			this->mPlayerProjectile[i].setProjectileColor(sf::Color(139, 0, 0, 255));
+	}
+
+	else if (this->pCurrentWeapon == ICE)
+	{
+		this->mPlayerProjectile.push_back(Projectile(&Player::mPlayerProjectileTextures[ICE], sf::Vector2f(this->mPlayerSprite.getPosition().x + 300, this->mPlayerSprite.getPosition().y + 195),
 			sf::Vector2f(-0.4F, 0.5F), sf::Vector2f(1.F, 0.F)));
 
-	else if (this->mCurrentWeapon == CORROSIVE)
-		this->mProjectile.push_back(Projectile(&Player::mPlayerProjectileTextures[CORROSIVE], sf::Vector2f(this->mPlayerSprite.getPosition().x + 300, this->mPlayerSprite.getPosition().y + 195),
+		for (size_t i = 0; i < this->mPlayerProjectile.size(); i++)
+			this->mPlayerProjectile[i].setProjectileColor(sf::Color::Blue);
+	}
+
+	else if (this->pCurrentWeapon == CORROSIVE)
+	{
+		this->mPlayerProjectile.push_back(Projectile(&Player::mPlayerProjectileTextures[CORROSIVE], sf::Vector2f(this->mPlayerSprite.getPosition().x + 300, this->mPlayerSprite.getPosition().y + 195),
 			sf::Vector2f(-0.4F, 0.5F), sf::Vector2f(1.F, 0.F)));
+
+		for (size_t i = 0; i < this->mPlayerProjectile.size(); i++)
+			this->mPlayerProjectile[i].setProjectileColor(sf::Color(124, 252, 0, 255));
+	}
 }
 
 void Player::loadProjectile()
@@ -99,25 +217,25 @@ void Player::loadProjectile()
 
 	this->mPlayerProjectileTextures.push_back(temp);
 
-	if (!temp.loadFromFile("Resources/Textures/Bullets/Fire1.png"))
+	if (!temp.loadFromFile("Resources/Textures/Bullets/Bullet_3.png"))
 		std::cerr << "The fucking bullet texture failed to fucking load" << "\n";
 
 	this->mPlayerProjectileTextures.push_back(temp);
 
-	if (!temp.loadFromFile("Resources/Textures/Bullets/Ice1.png"))
+	if (!temp.loadFromFile("Resources/Textures/Bullets/Bullet_3.png"))
 		std::cerr << "The fucking bullet texture failed to fucking load" << "\n";
 
 	this->mPlayerProjectileTextures.push_back(temp);
 
-	if (!temp.loadFromFile("Resources/Textures/Bullets/Corrosive1.png"))
+	if (!temp.loadFromFile("Resources/Textures/Bullets/Bullet_3.png"))
 		std::cerr << "The fucking bullet texture failed to fucking load" << "\n";
 
 	this->mPlayerProjectileTextures.push_back(temp);
 }
 
-void Player::updateAttack(const float & deltaTime)
+void Player::updateAttack(const float& deltaTime)
 {
-	if (sf::Mouse::isButtonPressed(sf::Mouse::Left) | sf::Keyboard::isKeyPressed(sf::Keyboard::Space) && this->mShootTimer >= this->mShootTimerMax && this->mKeyTime >= this->mKeyTimeMax)
+	if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && this->mShootTimer >= this->mShootTimerMax && this->mKeyTime >= this->mKeyTimeMax)
 	{
 		this->currentWeapon(deltaTime);
 
@@ -131,7 +249,7 @@ void Player::updateAttack(const float & deltaTime)
 
 		this->pAudio.play();
 
-		this->mProjectiles.createMuzzleFlash(sf::Vector2f(this->mPlayerSprite.getPosition().x + 150, this->mPlayerSprite.getPosition().y - 10), sf::Vector2f(0.6F, 0.5F));
+		this->mMuzzle.createMuzzleFlash(sf::Vector2f(this->mPlayerSprite.getPosition().x + 150, this->mPlayerSprite.getPosition().y - 10), sf::Vector2f(0.6F, 0.5F));
 	}
 }
 
@@ -148,6 +266,10 @@ void Player::createAnimationComponent(sf::Texture & texture_sheet)
 
 void Player::update(const float& deltaTime)
 {
+	this->auroa();
+
+	this->playerDealDamage();
+
 	this->mKeyTime += 1.F * deltaTime * 62.5F;
 
 	this->mShootTimer += 1.F * deltaTime * 62.5F;
@@ -158,13 +280,31 @@ void Player::update(const float& deltaTime)
 
 	this->updateAnimations(deltaTime);
 
-	for (size_t i = 0; i < this->mProjectile.size(); i++)
-		this->mProjectile[i].update(deltaTime);
+	for (size_t i = 0; i < this->mPlayerProjectile.size(); i++)
+		this->mPlayerProjectile[i].update(deltaTime);
 
-	this->mProjectiles.update(deltaTime);
+	this->mMuzzle.update(deltaTime);
+
+	this->frozen();
+
+	this->updatePlayerHpBar(deltaTime);
 }
 
-void Player::updateAnimations(const float & deltaTime)
+void Player::updateSoldier(const float& deltaTime)
+{
+	this->pMovementComponent->update(deltaTime);
+
+	this->animations(this->mSoldierSprite, deltaTime, pIsAttacking);
+
+	this->mSoldierSprite.move(1.F, 0.F);
+}
+
+void Player::renderSoldier(sf::RenderTarget & target)
+{
+	target.draw(this->mSoldierSprite);
+}
+
+void Player::updateAnimations(const float& deltaTime)
 {
 	if (this->IsAttacking())
 	{
@@ -215,12 +355,37 @@ void Player::updateAnimations(const float & deltaTime)
 	}
 }
 
-void Player::move(const float direction_x, const float direction_y, const float  & deltaTime)
+void Player::move(const float direction_x, const float direction_y, const float& deltaTime)
 {
 	if (this->pMovementComponent)
 		this->pMovementComponent->move(direction_x, direction_y, deltaTime);
 }
 
-void Player::startContact() { this->mIsContact = true; }
+void Player::auroa()
+{
+	if (this->mHp <= 4)
+	{
+		if (!this->mAuroaDyingTexture.loadFromFile("Resources/Textures/Aurora/auroaDying.png"))
+			std::cerr << "The fucking Aurora failed to fucking load" << "\n";
 
-void Player::endContact() { this->mIsContact = false; }
+		this->mAuroaDyingSprite.setTexture(this->mAuroaDyingTexture);
+		this->mAuroaDyingSprite.setPosition(this->mPlayerSprite.getPosition().x + 150, this->mPlayerSprite.getPosition().y - 10);
+		this->mAuroaDyingSprite.setRotation(45.F);
+		this->mAuroaDyingSprite.setScale(2.7F, 2.7F);
+		this->mAuroaDyingSprite.setColor(sf::Color::Black);
+	}
+}
+
+void Player::updatePlayerHpBar(const float& deltaTime)
+{
+	this->mHpBar1.setPosition(this->mPlayerSprite.getPosition().x + 100, this->mPlayerSprite.getPosition().y + 250);
+
+	if (this->mHp <= 9)
+		this->mHpBar1.setFillColor(sf::Color(220, 20, 60, 255));
+
+	if (this->mHp <= 12)
+		this->mHpBar1.setFillColor(sf::Color::Yellow);
+
+	if (this->mHp < 3)
+		this->mHpBar4.setFillColor(sf::Color::Black);
+}
--- Player.hpp
+++ Player.hpp
@@ -1,53 +1,87 @@
 #pragma once
-#include <Projectile/Projectile.hpp>
-#include <Component/AnimationComponent.hpp>
-#include <Component/MovementComponent.hpp>
-#include <Resources/Audio.hpp>
+#include <Entity/Entity.hpp>
 
-enum projectileType { REGULAR = 0, FIRE, ICE, CORROSIVE };
-
-class Player
+class Player : Entity
 {
 public:
 	Player();
 	~Player();
 
-	Projectile& getProjectile(unsigned index);
+	const int playerDealDamage() const;
 
+	void createTurret();
+	void updateTurret(const float& deltaTime);
+	void renderTurret(sf::RenderTarget& target);
+	void loadTurretProjectiles();
+	void createSoldier();
+	void takeDamage(int damage);
+	void removePlayerProjectile(unsigned index);
 	void render(sf::RenderTarget& target);
 	void update(const float& deltaTime);
+	void updateSoldier(const float& deltaTime);
+	void renderSoldier(sf::RenderTarget& target);
 	void updateAnimations(const float& deltaTime);
-	void move(const float direction_x, const float direction_y, const float  & deltaTime);
-	void startContact();
-	void endContact();
+	void move(const float direction_x, const float direction_y, const float& deltaTime);
+	void auroa();
+	void updatePlayerHpBar(const float& deltaTime);
 
-	inline const int getProjectileSize() { return this->mProjectile.size(); }
-	inline sf::Sprite getSprite() { return this->mPlayerSprite; }
+	//Tank Getter(s)
+	Projectile& getPlayerProjectile(unsigned index);
+	inline const int getPlayerProjectileSize() { return this->mPlayerProjectile.size(); }
+	inline sf::Sprite getPlayerSprite() { return this->mPlayerSprite; }
 	inline sf::FloatRect getGobalBounds()const { return this->mPlayerSprite.getGlobalBounds(); }
+	inline sf::Vector2f getPlayerCenter() { return this->mPlayerCenter; }
 	inline sf::Vector2f getPosition() const { return this->mPlayerSprite.getPosition(); }
+	inline const int getWeaponLevel() { return this->pCurrentWeapon; }
+	inline int getPlayerHp() { return this->mHp; }
 
-	bool IsAttacking() const { return mIsAttacking; }
+	//Tank Setter(s)
+	void setGunLevel(int level) { this->pCurrentWeapon = level; }
 
-protected:
-	MovementComponent* pMovementComponent;
-	AnimationComponent* pAnimationComponent;
-	Audio pAudio;
+	//Soldier Getter(s)
+	inline sf::Sprite getSoldierSprite() { return this->mSoldierSprite; }
+	inline sf::FloatRect getGobalBounds()const { return this->mSoldierSprite.getGlobalBounds(); }
+	inline sf::Vector2f getSoldierPosition() const { return this->mSoldierSprite.getPosition(); }
+
+	//Enable weapons
+	inline void enableFire() { this->pIsFireShot = true; }
+	inline void enableIce() { this->pIsIceShot = true; }
+	inline void enableCorrosive() { this->pIsCorrosiveShot = true; }
+
+	//Disable weapons
+	inline void disableFire() { this->pIsFireShot = false; }
+	inline void disableIce() { this->pIsIceShot = false; }
+	inline void disableCorrosive() { this->pIsCorrosiveShot = false; }
+
+	bool IsAttacking() const { return this->mIsAttacking; }
 
 private:
-	bool mIsContact;
+	//Damage
+	int mDamage;
+	int mDamageMax;
+
+	//HP
+	int mHp;
+	int mHpMax;
+
+	//Booleans
 	bool mIsAttacking;
 	bool mIsFiring;
 	bool mIsMuzzleOn;
-	
-	int mCurrentWeapon;
+	bool mIsAlive;
 
+	//Timers
 	float mMuzzleTimerMax;
 	float mMuzzleTimer;
+
 	float mKeyTimeMax;
 	float mKeyTime;
+
 	float mShootTimer;
 	float mShootTimerMax;
 
+	//Private shit
+	void hpBar();
 	void initVariables();
 	void currentWeapon(const float& deltaTime);
 	void loadProjectile();
@@ -56,17 +90,37 @@ private:
 		const float acceleration, const float deceleration);
 	void createAnimationComponent(sf::Texture& texture_sheet);
 
+	//Sprites & Textures
+	sf::RectangleShape mHpBar1;
+	sf::RectangleShape mHpBar2;
+	sf::RectangleShape mHpBar3;
+	sf::RectangleShape mHpBar4;
+
+	sf::Texture mAuroaDyingTexture;
+	sf::Sprite mAuroaDyingSprite;
+
+	//Tank
 	sf::Texture mPlayerTexture;
 	sf::Sprite mPlayerSprite;
+
+	//Soldier
+	sf::Texture mSoldierTexture;
+	sf::Sprite mSoldierSprite;
+
+	//Turret
+	sf::Texture mTurretTexture_0;
+	sf::Texture mTurretTexture_1;
+	sf::Sprite mTurretSprite_0;
+	sf::Sprite mTurretSprite_1;
 
 	sf::Vector2f mPlayerCenter;
 
 	sf::Sound mExplosion;
 	sf::SoundBuffer mBuffer;
 
-	Projectile mProjectiles;
-	std::vector<Projectile> mProjectile;
+	Projectile mMuzzle;
+	std::vector<Projectile> mPlayerProjectile;
 
 	static std::vector<sf::Texture> mPlayerProjectileTextures;
+	static std::vector<sf::Texture> mPlayerTurretProjectileTextures;
 };
-
