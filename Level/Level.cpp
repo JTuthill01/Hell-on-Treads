a89c939c3f1d0bd47d075d5b02d589637548ca31
Shadow <45497162+JTuthill01@users.noreply.github.com>
2019-05-02 13:11:50 -0500

    Add files via upload

--- Level/Level.cpp
+++ Level/Level.cpp
@@ -3,123 +3,71 @@
 
 std::vector<sf::Texture> Level::pParticleTextures;
 
-Level::Level(sf::RenderWindow* window, std::stack<Level*>* level) : pWindow(window), pLevel(level), pLoadLevel(false), pHasExploaded(false)
+Level::Level(sf::RenderWindow* window, std::stack<Level*>* level) : pWindow(window), pLevel(level), pLoadLevel(false), pHasExploaded(false), pIsPlayerProjectileRemoved(false), pShouldClose(false)
 {
 	this->pFont.loadFromFile("Resources/Fonts/Anton-Regular.ttf");
 }
 
-Level::~Level()
-{
-	while (!this->pLevel->empty())
-	{
-		delete this->pLevel->top();
+Level::~Level() = default;
 
-		this->pLevel->pop();
-	}
+void Level::removeTrees(unsigned index) { this->pTrees.erase(this->pTrees.begin() + index); }
+
+void Level::loadTrees()
+{
+	sf::Texture temp;
+	if (!temp.loadFromFile("Resources/Trees/trees.png"))
+		std::cerr << "Trees failed to load" << "\n";
+
+	this->pTree.treeTextures.push_back(temp);
 }
 
-void Level::removeEnemyPlane(unsigned index)
+void Level::endGameInput()
 {
-	this->mEnemyPlane.erase(this->mEnemyPlane.begin() + index);
+	if (pShouldClose == true)
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+			this->pWindow->close();
 }
 
-void Level::playerProjectileCollision(const float& deltaTime)
+void Level::endGame()
 {
-	if (this->pCollision.playerProjectileCollision(this->pPlayer, this->pEnemyTank.getEnemyTankSprite(), deltaTime))
-	{
-		this->setExplosions(sf::Vector2f(this->pEnemyTank.getEnemyTankPosition().x, this->pEnemyTank.getEnemyTankPosition().y), sf::Vector2f(2.F, 2.F));
+	this->mText.setFont(pFont);
+	this->mText.setCharacterSize(150U);
+	this->mText.setFillColor(sf::Color::Red);
+	this->mText.setPosition(sf::Vector2f(static_cast<float>(this->pWindow->getSize().x / 2) - 700.F, static_cast<float>(this->pWindow->getSize().y / 2) - 300.F));
+	this->mText.setString("GAME THE FUCK OVER!!!!!");
 
-		this->mAurora.setAurora(deltaTime);
+	this->pAudio.loadSound("Resources/Sounds/Game Over/Fuck.ogg");
+	this->pAudio.play();
 
-		this->mAurora.setAuroraPosition(sf::Vector2f(this->pEnemyTank.getEnemyTankPosition().x, this->pEnemyTank.getEnemyTankPosition().y + 100));
+	this->mResetText.setFont(pFont);
+	this->mResetText.setCharacterSize(100U);
+	this->mResetText.setFillColor(sf::Color::Blue);
+	this->mResetText.setPosition(sf::Vector2f(static_cast<float>(this->pWindow->getSize().x / 2.F) - 750.F, static_cast<float>(this->pWindow->getSize().y / 2.F) - 100.F));
+	this->mResetText.setString("Press Esc to quit "  " Press F1 to restart");
 
-		int damage = this->pPlayer.playerDealDamage();
+	this->pWindow->draw(this->mResetText);
 
-		if (this->pEnemyTank.getHp() > 0)
-			this->pEnemyTank.takeDamage(damage);
+	this->pWindow->draw(this->mText);
 
-		this->pTextTags.push_back(TextTags(&this->pFont, "BOOM!!!!", sf::Vector2f(this->pEnemyTank.getEnemyTankPosition().x + 100, this->pEnemyTank.getEnemyTankPosition().y + 100), sf::Vector2f(2.F, 2.F), sf::Color::Yellow,
-			40U));
-
-		this->pTextTags.push_back(TextTags(&this->pFont, "- " + std::to_string(damage), sf::Vector2f(this->pEnemyTank.getEnemyTankPosition().x + 100, this->pEnemyTank.getEnemyTankPosition().y), sf::Vector2f(1.F, 1.F), sf::Color::Yellow,
-			40U));
-	}
-
-	if (this->mExplosionTimer.isRunning())
-		this->pHasExploaded = true;
-
-	else if (this->mExplosionTimer.isExpired())
-		this->pHasExploaded = false;
-
-	if (this->pTextTagTimer.isExpired() && !this->pTextTags.empty())
-		this->pTextTags.pop_back();
+	this->pShouldClose = true;
 }
 
-void Level::enemyProjectileCollision(const float& deltaTime)
+void Level::renderEndGame(sf::RenderTarget & target)
 {
-	if (this->pCollision.enemyProjectileCollision(this->pEnemyTank, this->pPlayer.getPlayerSprite(), deltaTime))
-	{
-		this->mAurora.setEnemyAurora(deltaTime);
+	target.draw(this->mText);
 
-		this->mAurora.setEnemyAuroraPosition(sf::Vector2f(this->pPlayer.getPosition().x - 150, this->pPlayer.getPosition().y));
-
-		this->setExplosions(sf::Vector2f(this->pPlayer.getPlayerSprite().getPosition().x, this->pPlayer.getPlayerSprite().getPosition().y), sf::Vector2f(2.F, 2.F));
-
-		int damage = this->pEnemyTank.enemyDealDamage();
-
-		if (this->pPlayer.getPlayerHp() > 0)
-			this->pPlayer.takeDamage(damage);
-
-		this->pTextTags.push_back(TextTags(&this->pFont, "BOOM!!!!", sf::Vector2f(this->pPlayer.getPosition().x + 100, this->pPlayer.getPosition().y), sf::Vector2f(1.F, 2.F), sf::Color::Yellow,
-			40U));
-
-		this->pTextTags.push_back(TextTags(&this->pFont, "- " + std::to_string(damage), sf::Vector2f(this->pPlayer.getPosition().x + 100, this->pPlayer.getPosition().y + 50), sf::Vector2f(1.F, 2.F), sf::Color(178, 34, 34, 240),
-			40U));
-	}
-
-	if (this->mExplosionTimer.isRunning())
-		this->pHasExploaded = true;
-
-	else if (this->mExplosionTimer.isExpired())
-		this->pHasExploaded = false;
-
-	if (this->pTextTagTimer.isExpired() && !this->pTextTags.empty())
-		this->pTextTags.pop_back();
+	target.draw(this->mResetText);
 }
 
-void Level::playerInput(const float& deltaTime)
-{
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-		this->pPlayer.move(0.45F, 0.F, deltaTime);
+void Level::removeEnemyPlane(unsigned index) { this->pEnemyPlane.erase(this->pEnemyPlane.begin() + index); }
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-		this->pPlayer.move(-0.8F, 0.F, deltaTime);
-}
 
 void Level::updateLevel(const float& deltaTime)
 {
 	for (size_t i = 0; i < this->pParticle.size(); i++)
 		this->pParticle[i].update(deltaTime);
 
-	this->pPlayer.update(deltaTime);
-
-	this->playerInput(deltaTime);
-
 	this->pEnemyTank.update(deltaTime);
-
-	this->playerProjectileCollision(deltaTime);
-
-	this->enemyProjectileCollision(deltaTime);
-
-	this->pCollision.removePlayerProjectile(this->pPlayer, this->pWindow, deltaTime);
-
-	this->pCollision.removeEnemyProjectile(this->pWindow, this->pEnemyTank, deltaTime);
-
-	this->pCollision.playerEnemyCollision(this->pPlayer.getPlayerSprite(), this->pEnemyTank.getEnemyTankSprite(), deltaTime);
-
-	this->pCollision.playerProjectileCollision(this->pPlayer, this->pEnemyTank.getEnemyTankSprite(), deltaTime);
-
-	this->pCollision.enemyProjectileCollision(this->pEnemyTank, this->pPlayer.getPlayerSprite(), deltaTime);
 }
 
 void Level::setExplosions(sf::Vector2f position, sf::Vector2f scale)
--- Level/Level.hpp
+++ Level/Level.hpp
@@ -1,5 +1,12 @@
 #pragma once
+#include <Collision/PixelCollision.hpp>
+#include <Resources/Tree.hpp>
+#include <Weather/Clouds.hpp>
+#include <Resources/Audio.hpp>
+#include <Weather/Weather.hpp>
+#include <Projectile/Projectile.hpp>
 #include <Resources/Particle.hpp>
+#include <Entity/Plane/Plane.hpp>
 #include <Entity/Player/Player.hpp>
 #include <Entity/Enemies/Enemy.hpp>
 #include <Collision/Collision.hpp>
@@ -11,40 +18,60 @@ class Level
 {
 public:
 	Level(sf::RenderWindow* window, std::stack<Level*>* level);
-	~Level();
+	virtual ~Level();
 
 	virtual void update(const float& deltaTime) = 0;
 	virtual void render(sf::RenderTarget& target) = 0;
 	virtual void initLevel() = 0;
+	virtual void removeProjectile() = 0;
 
 protected:
+	void removeTrees(unsigned index);
+	void loadTrees();
+	void endGameInput();
+	void endGame();
+	void renderEndGame(sf::RenderTarget& target);
 	void removeEnemyPlane(unsigned index);
-	void playerProjectileCollision(const float& deltaTime);
-	void enemyProjectileCollision(const float& deltaTime);
-	void playerInput(const float& deltaTime);
 	void updateLevel(const float& deltaTime);
 	void setExplosions(sf::Vector2f position, sf::Vector2f scale);
 
 	std::stack<Level*>* pLevel;
 
+	sf::Event e;
+	sf::Texture pGroundTexture;
 	sf::Texture pBackgroundTexture;
 	sf::Texture mExpolsionTexture;
+	sf::Sprite pGroundSprite;
 	sf::Sprite pBackgroundSprite;
 	sf::Sprite mExpolsionSprite;
 	sf::RenderWindow* pWindow;
 	sf::Font pFont;
+	std::vector<sf::Texture> pTextures;
+	std::vector<sf::Texture> pProjectileTextures;
+	std::vector<sf::Texture> pTreeTextures;
 
 	//Timers
 	thor::Timer pEnemySpawnTimer;
 	thor::Timer pTextTagTimer;
+	thor::Timer pCloudSpawnTimer;
 
+	Plane pPlayerPlane;
 	Player pPlayer;
 	std::vector<Player> player;
 
 	Enemy pEnemyTank;
-	std::vector<Enemy> enemyTank;
-	std::vector<Enemy> mEnemyPlane;
 
+	std::vector<Trees::Tree> pTrees;
+	std::vector<Enemy> pEnemyJeep;
+	std::vector<Enemy> pEnemyTanks;
+	std::vector<Enemy> pEnemyPlane;
+	std::vector<Projectile>pJeepProjectiles;
+	std::vector<Projectile> pProjectiles;
+	std::vector<TextTags> pTextTags;
+
+	Trees::Tree pTree; 
+	Audio pAudio;
+	Weather pWeather;
 	Entity pEntity;
 	Collision pCollision;
 	AuroraLoader::AuroraLoader mAurora;
@@ -52,13 +79,21 @@ protected:
 	static std::vector<sf::Texture> pParticleTextures;
 	std::vector<Particle::Particle> pParticle;
 
+	bool pShouldClose;
+	bool pIsTreeRemoved;
+	bool pIsEnemyProjectileRemoved;
+	bool pIsPlayerProjectileRemoved;
 	bool pLoadLevel;
 	bool pHasExploaded;
 
-	std::vector<TextTags> pTextTags;
-
-private:
 	thor::Timer mExplosionTimer;
 	thor::Timer mParticleTimer;
 	void timer();
+
+private:
+	sf::Text mText;
+	sf::Text mResetText;
+	
+	thor::Timer mReset;
+	
 };
